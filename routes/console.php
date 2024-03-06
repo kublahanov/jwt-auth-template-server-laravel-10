de40850b9f9d34d608c41663d75bcc4fcce3e128
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-03-07 00:23:29 +0300

    Add console command to display only user-defined commands.

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,31 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/**
+ * Вывод таблицы с пользовательскими командами
+ * (наследуются от класса App\Console\Commands).
+ */
+Artisan::command('x', function () {
+    $list = collect(Artisan::all())
+        ->filter(function ($value, $key) {
+            return str_starts_with($key, 'x-');
+        })
+        ->transform(function ($value, $key) {
+            return [
+                "command" => $key,
+                "description" => $value->getDescription(),
+            ];
+        })
+        ->toArray()
+    ;
+
+    $this->newLine();
+    $this->line('List of user-defined commands:');
+
+    $this->table(
+        ['Command', 'Description'],
+        $list,
+        'box',
+    );
+})->purpose('Display only user-defined commands');

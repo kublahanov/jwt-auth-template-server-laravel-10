47264b9c76048e0a38ce738c9efc9a94ca6151d8
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-03-11 20:56:01 +0300

    Set queue options to database.

---
+++ database/migrations/2024_03_11_204315_create__jobs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @var string
+     */
+    private $tableName;
+
+    /**
+     * Getting table name.
+     */
+    public function __construct()
+    {
+        $this->tableName =  config('queue.connections.database.table');
+    }
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+};

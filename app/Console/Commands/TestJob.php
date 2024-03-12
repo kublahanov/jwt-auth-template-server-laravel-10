54a81e47dc4e39637571de3ef3bf60ef98a8c5fd
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-03-12 22:08:34 +0300

    Add queue test job and console command.

---
+++ app/Console/Commands/TestJob.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Console\Command;
+use App\Jobs\TestJob as JobTestJob;
+use Illuminate\Support\Carbon;
+
+/**
+ * Тест выполнения фонового задания.
+ */
+class TestJob extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'test:job';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Тест выполнения фонового задания';
+
+    /**
+     * Execute the console command.
+     * @return int
+     */
+    public function handle()
+    {
+        $this->newLine();
+        $this->alert($this->description);
+
+        $startTime = Carbon::now();
+
+        JobTestJob::dispatch();
+
+        // $this->newLine();
+        // $this->alert('alert'); // Жёлтый
+        // $this->info('info'); // Зелёный
+        // $this->line('line'); // Белый
+        // $this->comment('comment'); // Жёлтый
+        // $this->question('question'); // Голубой
+        // $this->error('error'); // Красный
+        // $this->warn('warn'); // Жёлтый
+
+        $duration = (Carbon::now())->diffInSeconds($startTime);
+        $this->info("Время выполнения (сек.): {$duration}.");
+
+        return 0;
+    }
+}

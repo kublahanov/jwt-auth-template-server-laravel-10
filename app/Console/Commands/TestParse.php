aa710bc5ac129a64832d0341b1714ab5f0f18cfc
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-03-14 23:08:50 +0300

    Add TestParse and runtime folder.

---
+++ app/Console/Commands/TestParse.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Console\Command;
+use Exception;
+use Illuminate\Support\Carbon;
+
+/**
+ * Тест парсера файлов.
+ */
+class TestParse extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'test:parse';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Тест парсера файлов';
+
+    /**
+     * Execute the console command.
+     * @return int
+     */
+    public function handle()
+    {
+        $this->newLine();
+        $this->alert($this->description);
+
+        $startTime = Carbon::now();
+
+        $directory = storage_path("/runtime/parse");
+        $files = $this->getFilesList($directory);
+
+        dump($files);
+
+        $this->newLine();
+
+        foreach ($files as $fileName) {
+            $fullPath = "$directory/$fileName";
+            $this->line("Try to parse: $fullPath.");
+
+            $this->line("YAML:");
+            try {
+                var_dump(yaml_parse_file($fullPath));
+            } catch (Exception $exception) {
+                $this->info($exception->getMessage());
+            }
+
+            $this->line("INI:");
+            try {
+                var_dump(parse_ini_file($fullPath));
+            } catch (Exception $exception) {
+                $this->info($exception->getMessage());
+            }
+        }
+
+        $this->newLine();
+
+        // $this->newLine();
+        // $this->alert('alert'); // Жёлтый
+        // $this->info('info'); // Зелёный
+        // $this->line('line'); // Белый
+        // $this->comment('comment'); // Жёлтый
+        // $this->question('question'); // Голубой
+        // $this->error('error'); // Красный
+        // $this->warn('warn'); // Жёлтый
+
+        $duration = (Carbon::now())->diffInSeconds($startTime);
+        $this->info("Время выполнения (сек.): {$duration}.");
+
+        return 0;
+    }
+
+    /**
+     * @param $directory
+     * @return array|false
+     */
+    protected function getFilesList($directory)
+    {
+        $fileList = [];
+
+        if (!is_dir($directory)) {
+            return false;
+        }
+
+        $files = scandir($directory);
+
+        foreach ($files as $file) {
+            if ($file !== '.' && $file !== '..') {
+                if (is_file($directory . '/' . $file)) {
+                    $fileList[] = $file;
+                }
+            }
+        }
+
+        return $fileList;
+    }
+}

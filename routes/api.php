d44480aa409277415a34e692cc074aca87c723fe
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-07-30 18:49:56 +0300

    Optimize routes list.

--- routes/api.php
+++ routes/api.php
@@ -24,25 +24,25 @@ Route::get('/test/migrations', [TestController::class, 'migrations'])->name('tes
 /**
  * AuthController.
  */
-Route::prefix('auth')->group(function ($router) {
-    Route::post('register', [AuthController::class, 'register'])
+Route::prefix('auth')->controller(AuthController::class)->group(function ($router) {
+    Route::post('register', 'register')
         ->name(AuthService::AUTH_ROUTES_NAMES['register']);
-    Route::get('verify-email/{id}/{hash}', [AuthController::class, 'verifyEmail'])
+    Route::get('verify-email/{id}/{hash}', 'verifyEmail')
         ->middleware(['signed', 'decode.hash'])
         ->name(AuthService::AUTH_ROUTES_NAMES['verify-email']);
 
-    Route::post('login', [AuthController::class, 'login'])
+    Route::post('login', 'login')
         ->name(AuthService::AUTH_ROUTES_NAMES['login']);
-    Route::post('logout', [AuthController::class, 'logout'])
+    Route::post('logout', 'logout')
         ->name(AuthService::AUTH_ROUTES_NAMES['logout']);
-    Route::post('refresh', [AuthController::class, 'refresh'])
+    Route::post('refresh', 'refresh')
         ->name(AuthService::AUTH_ROUTES_NAMES['refresh']);
 
-    Route::get('me', [AuthController::class, 'me'])
+    Route::get('me', 'me')
         ->name(AuthService::AUTH_ROUTES_NAMES['me']);
 
-    Route::post('send-reset-password-link', [AuthController::class, 'sendResetPasswordLink'])
+    Route::post('send-reset-password-link', 'sendResetPasswordLink')
         ->name(AuthService::AUTH_ROUTES_NAMES['send-reset-password-link']);
-    Route::post('reset-password', [AuthController::class, 'resetPassword'])
+    Route::post('reset-password', 'resetPassword')
         ->name(AuthService::AUTH_ROUTES_NAMES['reset-password']);
 });

0dd0944dec469598c36c534abdb42461bfadea92
Roman Kondrashov <nr.kondrashov@gmail.com>
2025-06-21 20:52:35 +0300

    Исправления Dockerfile для PHP версии 8.3.

--- docker/8.3/docker-entrypoint.sh
+++ docker/8.3/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Проверяем версию PHP
+CURRENT_PHP=$(php -v | head -n 1 | cut -d' ' -f2 | cut -d'.' -f1-2)
+if [ "$CURRENT_PHP" != "8.3" ]; then
+    echo "ERROR: Wrong PHP version detected ($CURRENT_PHP). Forcing PHP 8.3..."
+    update-alternatives --set php /usr/bin/php8.3
+    exec php "$0" "$@"
+fi
+
 # Создаем директории, если их нет (для работы без volume)
 mkdir -p /var/www/html/storage/{logs,framework/cache,framework/views}
 chown -R sail:sail /var/www/html/storage

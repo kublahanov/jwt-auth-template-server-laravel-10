02fbde7118e77af384109cec6fc7cd769e78b7a3
Roman Kondrashov <nr.kondrashov@gmail.com>
2025-06-22 16:33:10 +0300

    Фиксы для корректной установки.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Setup composer dependencies
-composer install
-
 # Making .env from .env.example, if not yet
-if [ ! -f .env ]; then
-    cp .env.example .env
-fi
+#if [ ! -f .env ]; then
+#    cp .env.example .env
+#fi
 
 # Starting Docker stack
 ./vendor/bin/sail up -d
 
+# Setup composer dependencies
+./vendor/bin/sail composer install
+
 # Generating application key
-./vendor/bin/sail artisan key:generate --ansi
+#./vendor/bin/sail artisan key:generate --ansi
 
 # Starting migrations and seeds
 ./vendor/bin/sail artisan migrate --seed

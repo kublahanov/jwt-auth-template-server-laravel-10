da3577d31a54aa1462e3e8b49a8588d855d05779
Roman Kondrashov <nr.kondrashov@gmail.com>
2025-06-22 16:32:31 +0300

    Защита сидеров от перезапуска.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,23 +4,33 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
+    protected $admin = [
+        'name' => 'Кондрашов Роман Викторович',
+        'email' => '4progs@inbox.ru',
+        'password' => '1234567',
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Кондрашов Роман Викторович',
-            'email' => '4progs@inbox.ru',
-            'password' => '1234567',
-        ]);
-
-        User::factory(12)
-            ->unverified()
-            ->create()
+        $exists = DB::table(User::getTableName())
+            ->where('email', $this->admin['email'])
+            ->exists()
         ;
+
+        if (!$exists) {
+            User::factory()->create($this->admin);
+
+            User::factory(12)
+                ->unverified()
+                ->create()
+            ;
+        }
     }
 }
